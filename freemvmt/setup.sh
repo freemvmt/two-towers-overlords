4250483733e44cd775d391f635c6877e48a296cb
Dan G <freemvmt@posteo.net>
2025-06-17 16:53:41 +0100

    add .env example, add sweep trajectories png, stop parallelism for tokenization

---
+++ freemvmt/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# run like `source setup.sh` to ensure active shell is set up with venv
+apt update
+# ensure we have all the utils we need
+apt install -y vim rsync git nvtop htop tmux curl git-lfs
+apt upgrade -y
+# install uv and sync
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+uv sync
+# load env vars into shell
+cp .env.example .env
+set -a
+source .env
+set +a
+# activate virtual environment for running python scripts
+source .venv/bin/activate
