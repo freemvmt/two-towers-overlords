815a0cf23dc2c70c6696f221d6d80286f7cc1183
Dan G <freemvmt@posteo.net>
2025-06-19 19:31:37 +0100

    add GET_DOCKER functionality to setup.sh

--- freemvmt/setup.sh
+++ freemvmt/setup.sh
@@ -2,9 +2,9 @@
 # run like `source setup.sh` on any remote to ensure active shell is set up with venv
 
 # ensure we have all the utils we need
-apt update
-apt install -y vim rsync git nvtop htop tmux curl git-lfs
-apt upgrade -y
+apt-get update
+apt-get install -y vim rsync git nvtop htop tmux curl ca-certificates git-lfs
+apt-get upgrade -y
 
 # load env vars into shell
 cp .env.example .env
@@ -33,3 +33,17 @@ fi
 
 # activate virtual environment for running python scripts
 source .venv/bin/activate
+
+# finally, if GET_DOCKER env var is set, install it
+if [[ "$GET_DOCKER" == "1" ]]; then
+  echo "🐳 GET_DOCKER enabled - installing Docker for the server"
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  chmod a+r /etc/apt/keyrings/docker.asc
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt-get update
+  apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
