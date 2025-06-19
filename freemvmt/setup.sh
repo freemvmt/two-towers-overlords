17142e8c8c56a7675ed69a467550582585401a72
Dan G <freemvmt@posteo.net>
2025-06-19 19:58:36 +0100

    run redis directly on remote (cant do docker-in-docker on runpod), more logs for ingest pipeline

--- freemvmt/setup.sh
+++ freemvmt/setup.sh
@@ -31,19 +31,12 @@ else
   uv sync
 fi
 
-# activate virtual environment for running python scripts
-source .venv/bin/activate
-
-# finally, if GET_DOCKER env var is set, install it
-if [[ "$GET_DOCKER" == "1" ]]; then
-  echo "🐳 GET_DOCKER enabled - installing Docker for the server"
-  install -m 0755 -d /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  chmod a+r /etc/apt/keyrings/docker.asc
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-    tee /etc/apt/sources.list.d/docker.list > /dev/null
-  apt-get update
-  apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# if RUN_REDIS env var is set, run a deamonized redis server on the remote
+if [[ "$RUN_REDIS" == "1" ]]; then
+  echo "🚀 RUN_REDIS is set - installing and starting Redis server in the background"
+  apt-get install -y redis-server
+  redis-server --daemonize yes
 fi
+
+# finally, activate virtual environment for running python scripts
+source .venv/bin/activate
