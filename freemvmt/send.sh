f3a0d50bff62af41fb78333eecc3e85077920054
Dan G <freemvmt@posteo.net>
2025-06-19 11:51:36 +0100

    finesse scripts

---
+++ freemvmt/send.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# run like `./send.sh` on local, to prepare remote to run ssh.sh
+
+if [[ -z "${1-}" ]]; then
+    REMOTE="mlx"
+else
+    REMOTE="$1"
+fi
+
+scp ~/.ssh/id_ed25519 "$REMOTE":~/.ssh/id_ed25519"
+scp ssh.sh "$REMOTE":ssh.sh"
--- freemvmt/ssh.sh
+++ freemvmt/ssh.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-# run like `./ssh.sh` (on tmp remote, after sending private key) to set up ssh agent and pull down repo
+# run like `./ssh.sh` on tmp runpod, after sending your private key and this script
+# will set up ssh agent and pull down repo into /workspace dir
 
 eval "$(ssh-agent -s)"
 ssh-add .ssh/id_ed25519
+cd /workspace
 git clone git@github.com:freemvmt/two-towers-overlords.git
+cd two-towers-overlords
